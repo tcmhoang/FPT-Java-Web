74cdd36980286a55a957eda7ad96342e1c2607f2
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-09-19 00:00:10 -0400

    Finished adding Pinterst header and changed the struct of the page

--- Pinterest/script.js
+++ Pinterest/script.js
@@ -1,89 +1,90 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const API_URL = "";
+// document.addEventListener("DOMContentLoaded", function (event) {
+const API_URL = "";
 
-  var ms_cols = Array.from(document.querySelectorAll(".col"));
+var ms_cols = Array.from(document.querySelectorAll(".col"));
 
-  var sheet = window.document.styleSheets[0];
+var sheet = window.document.styleSheets[0];
 
-  sheet.insertRule(
-    `.col{flex:${(1 / ms_cols.length) * 0.9}`,
-    sheet.cssRules.length
+sheet.insertRule(
+  `.col{flex:${(1 / ms_cols.length) * 0.9}`,
+  sheet.cssRules.length
+);
+
+var image_heights = [
+  120,
+  130,
+  140,
+  150,
+  160,
+  170,
+  180,
+  190,
+  200,
+  210,
+  220,
+  230,
+  240,
+  250,
+  260,
+  270,
+  280,
+  290,
+  300,
+  310,
+  320,
+  330,
+  340,
+  350,
+  360,
+];
+
+function loadTiles(row_count) {
+  for (let i = 0; i < row_count * row_count; i++) {
+    let tile_height =
+      image_heights[Math.floor(Math.random() * image_heights.length)];
+    const current_col = getMinHeightCol(ms_cols);
+
+    const tile = document.createElement("figure");
+    tile.setAttribute("class", "tile");
+
+    const thumbnail = document.createElement("div");
+    thumbnail.setAttribute("class", "thumbnail");
+    const rand = Math.floor(Math.random() * 256);
+    thumbnail.style.backgroundColor = `rgb(${rand}, ${128}, ${256 - rand})`;
+
+    const img = document.createElement("img");
+    img.src = "https://picsum.photos/200/" + tile_height;
+    img.alt = "Very cool picture";
+    img.addEventListener("load", (event) => (img.width = img.naturalWidth));
+
+    const overlay = document.createElement("a");
+    overlay.setAttribute("class", "overlay");
+
+    thumbnail.style.height = tile_height + "px";
+
+    thumbnail.appendChild(img);
+    thumbnail.appendChild(overlay);
+    tile.appendChild(thumbnail);
+
+    current_col.appendChild(tile);
+  }
+}
+
+function getMinHeightCol(colList) {
+  return colList.reduce((curr, next) =>
+    curr.scrollHeight > next.scrollHeight ? next : curr
   );
+}
 
-  var image_heights = [
-    120,
-    130,
-    140,
-    150,
-    160,
-    170,
-    180,
-    190,
-    200,
-    210,
-    220,
-    230,
-    240,
-    250,
-    260,
-    270,
-    280,
-    290,
-    300,
-    310,
-    320,
-    330,
-    340,
-    350,
-    360,
-  ];
+loadTiles(4);
 
-  function loadTiles(row_count) {
-    for (let i = 0; i < row_count * row_count; i++) {
-      let tile_height =
-        image_heights[Math.floor(Math.random() * image_heights.length)];
-      const current_col = getMinHeightCol(ms_cols);
-
-      const tile = document.createElement("figure");
-      tile.setAttribute("class", "tile");
-
-      const thumbnail = document.createElement("div");
-      thumbnail.setAttribute("class", "thumbnail");
-      const rand = Math.floor(Math.random() * 256);
-      thumbnail.style.backgroundColor = `rgb(${rand}, ${128}, ${256 - rand})`;
-
-      const img = document.createElement("img");
-      img.src = "https://picsum.photos/200/" + tile_height;
-      img.alt = "Very cool picture";
-
-      const overlay = document.createElement("a");
-      overlay.setAttribute("class", "overlay");
-
-      thumbnail.style.height = tile_height + "px";
-
-      thumbnail.appendChild(img);
-      thumbnail.appendChild(overlay);
-      tile.appendChild(thumbnail);
-
-      current_col.appendChild(tile);
-    }
+var container = document.querySelector(".main");
+container.addEventListener("scroll", function () {
+  if (
+    Math.ceil(container.scrollTop) + container.clientHeight >=
+    container.scrollHeight
+  ) {
+    setTimeout(() => loadTiles(3), 1500);
   }
-
-  function getMinHeightCol(colList) {
-    return colList.reduce((curr, next) =>
-      curr.scrollHeight > next.scrollHeight ? next : curr
-    );
-  }
-
-  loadTiles(4);
-
-  var container = document.querySelector(".container");
-  container.addEventListener("scroll", function () {
-    if (
-      Math.ceil(container.scrollTop) + container.clientHeight >=
-      container.scrollHeight
-    ) {
-      setTimeout(() => loadTiles(3), 1500);
-    }
-  });
 });
+// });
