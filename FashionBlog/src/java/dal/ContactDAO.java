582b8c1469b64bc72dbcbe3d0bab5300d437aa35
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-10-04 20:59:09 -0400

    Finish Entries DAO with Hibernate

---
+++ FashionBlog/src/java/dal/ContactDAO.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import domain.Contact;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Camer
+ */
+public class ContactDAO {
+
+    private static Session session = null;
+    private static Transaction transaction = null;
+
+    public static void insert(Contact contact) {
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(contact);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+            session = null;
+            transaction = null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        insert(new Contact(1, "Foo", "bar", "ahihi"));
+    }
+}
---
+++ FashionBlog/src/java/dal/EntryDAO.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.util.List;
+import domain.Entries;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.transform.Transformers;
+
+/**
+ *
+ * @author Camer
+ */
+public class EntryDAO {
+
+    private static Session session = null;
+    private static Transaction transaction = null;
+
+    public static List<Entries> selectTop(int top) {
+        List<Entries> entries = null;
+        String sql = "FROM Entries\n"
+                + "ORDER BY Published DESC";
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery(sql);
+            entries = query.setMaxResults(top).list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            session = null;
+        }
+        return entries;
+    }
+
+    public static List<Entries> fetchAll() {
+        List<Entries> entries = null;
+        String sql = "FROM Entries ORDER BY Published DESC";
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery(sql);
+            entries = query.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            session = null;
+        }
+        return entries;
+    }
+
+    public static Entries selectByEntryID(int entryID) {
+        String sql = "FROM Entries\n"
+                + "WHERE EntryID= :entryid \n"
+                + "ORDER BY Published DESC";
+        Entries res = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery(sql);
+            query.setParameter("entryid", entryID);
+            res = (Entries) query.uniqueResult();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            session = null;
+        }
+        return res;
+    }
+
+}
