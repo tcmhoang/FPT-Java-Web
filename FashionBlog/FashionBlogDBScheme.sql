884a1657d8410462db820e6b85be7c75c738926a
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-10-08 06:16:16 -0700

    Fix database syntax

--- FashionBlog/FashionBlogDBScheme.sql
+++ FashionBlog/FashionBlogDBScheme.sql
@@ -62,7 +62,7 @@ INSERT INTO Entries
 VALUES
 	(1, 'Special Offer', GETDATE(), 'i284852689410723270._szw1280h1280_.jpg', 'Claritas est etiam processus dynamicus, qui sequitur');
 
-INSERT Information
+INSERT INTO Information
 	(name, age, img, description)
 VALUES
 	( 'Susan Stratton', 25, 'i284852689410723613._szw480h1280_.jpg', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.');
